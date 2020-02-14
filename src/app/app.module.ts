98b291ee89480c49d747838d121ff39ffca88c1c
Jake <J.atahanov@yahoo.com>
2020-02-14 17:19:42 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
+import { TablesExampleComponent } from './tables-example/tables-example.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// import{ MatButtonMOdule, MatFormFieldModule, MatIconModule, MatInputModule,
+  // MatListModule, MatSelectModule, MatSideNavModule, MatCardModule, MatMenuModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablesExampleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // MatButtonMOdule,
+    // MatFormFieldModule, 
+    // MatIconModule, 
+    // MatInputModule,
+    // MatListModule, 
+    // MatSelectModule, 
+    // MatSideNavModule, 
+    // MatCardModule, 
+    MatTableModule
+    // MatMenuModule, 
+    // MatPaginatorModule, 
+    // MatSortModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
